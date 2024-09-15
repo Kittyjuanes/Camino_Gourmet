1456711a9f88047f32769ccccf04b4ddaa06354b
dsolisav <dsolis@unal.edu.co>
2024-09-15 15:05:19 -0500

    Avances en pantallas

--- Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/MiRestaurante.kt
+++ Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/MiRestaurante.kt
@@ -10,12 +10,7 @@ import com.example.camino_gourmet.R
 class MiRestaurante : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_mi_restaurante)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
--- Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/Perfil.kt
+++ Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/Perfil.kt
@@ -10,12 +10,7 @@ import com.example.camino_gourmet.R
 class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
