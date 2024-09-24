b8668e24ad88181a8d3743f39235b13cff97fcd2
dsolisav <dsolis@unal.edu.co>
2024-09-24 02:04:36 -0500

    Pantalla para calificar restaurante

--- Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/CalificarRestaurante.kt
+++ Camino_Gourmet/Camino_Gourmet/app/src/main/java/com/example/camino_gourmet/logic/CalificarRestaurante.kt
@@ -1,8 +1,10 @@
 package com.example.camino_gourmet.logic
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.camino_gourmet.R
@@ -10,12 +12,11 @@ import com.example.camino_gourmet.R
 class CalificarRestaurante : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_calificar_restaurante)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val layout = findViewById<ConstraintLayout>(R.id.layoutSubirImagen)
+        layout.setBackgroundResource(R.drawable.rounded_corners)
+        layout.background.setTint(Color.parseColor("#f2f2f2"))
+
     }
 }
